7ab72b5ae96ee18bf52f3b8cf0c75cc96cde30f7
SirBlobman <blobmansrealm@gmail.com>
2021-08-22 23:01:59 -0400

    Remove example method from main class.

--- src/main/java/com/github/sirblobman/modlistmod/ModListMod.java
+++ src/main/java/com/github/sirblobman/modlistmod/ModListMod.java
@@ -1,12 +1,11 @@
 package com.github.sirblobman.modlistmod;
 
-import net.minecraft.init.Blocks;
+import net.minecraft.command.ICommand;
 
 import com.github.sirblobman.modlistmod.command.CommandMods;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = ModListMod.MODID, version = ModListMod.VERSION, acceptableRemoteVersions = "*")
@@ -15,13 +14,8 @@ public final class ModListMod {
     public static final String VERSION = "1.0.0";
     
     @EventHandler
-    public void init(FMLInitializationEvent event) {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-    }
-    
-    @EventHandler
-    public void serverStart(FMLServerStartingEvent event) {
-        event.registerServerCommand(new CommandMods(this));
+    public void onServerStarting(FMLServerStartingEvent event) {
+        ICommand commandMods = new CommandMods(this);
+        event.registerServerCommand(commandMods);
     }
 }
